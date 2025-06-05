fdec8a9bb5b18dd0ca65b5c23ab279c6e55efcf9
GeorgeKreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-06-05 18:49:28 -0400

    Task 4+5

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -22,6 +22,28 @@ def submit(request, course_id):
     submission_id = submission.id
     return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id,submission_id)))
 
+def show_exam_result(request,course_id,submission_id):
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+
+    total_score = 0
+    questions = course.question_set.all() # Assuming course has related questions
+    for question in questions:
+        correct_choices = question.RELATION_SET.filter(is_correct=True)
+        selected_choices = choices.filter(question=question)
+
+        #Check if the selected choices are the correct choices
+        if set(correct_choices) == set(selected_choices):
+            total_score = question.grade
+
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+
 def registration_request(request):
     context = {}
     if request.method == 'GET':
